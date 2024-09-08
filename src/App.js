4b5c3062f70f028cb9f7ad095e962dca0a47211b
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-09-08 16:13:26 +0300

    added home & navbar components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import ScrollToTop from './components/ScrollToTop'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Free from './components/Free'
+import Clients from './components/Clients'
+import SuperRare from './components/SuperRare'
+import Release from './components/Release'
+import Like from './components/Like'
+import Signup from './components/Signup'
+import Footer from './components/Footer'
+import './scss/index.scss'
 
 export default function App() {
-  return <div>hello</div>
+  return (
+    <div className="app-container">
+      <ScrollToTop />
+      <Navbar />
+      <Home />
+      <Free />
+      <Clients />
+      <SuperRare />
+      <Release />
+      <Like />
+      <Signup />
+      <Footer />
+    </div>
+  )
 }
---
+++ src/components/SuperRare.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SuperRare(){
+    return <div></div>;
+}
