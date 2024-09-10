8638586fe0e5b6fc83ff5f3a2e80fdf78d4ab386
shelep-olya <142796512+shelep-olya@users.noreply.github.com>
2024-09-10 19:37:55 +0300

    added releases, superRare & clients sections

--- src/components/Release.jsx
+++ src/components/Release.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import release1 from "../assets/release1.png";
+import release2 from "../assets/release2.png";
+import Card from "./Card";
 
 export default function Release(){
-    return <div></div>
+    return <div className="releases">
+        <div className="release orange">
+            <div className="content">
+                <h2 className="title">Intital Release 4/11</h2>
+                <p className="description">We have released four limited edition NFT's early which can be bid via  <a href="#">OpenSea.</a></p>
+                <p className="description">There will be the only four of NFT's we ever make, so be sure not to miss out!</p>
+                <p className="description">50% of proceeds go to charity.</p>
+                <a href="#" className="link">check them out <BsArrowRight /></a>
+            </div>
+            <div className="image">
+                <img src={release1} alt="release" />
+                <div className="ellipse pink"></div>
+            </div>
+        </div>
+        <div className="release green">
+        <div className="card-container">
+                <Card
+                image={release2}
+                series="Floop Series"
+                title="Purple Man"
+                price={2.99} tag={12983} time={1}
+                />
+                <div className="ellipse orange"></div>
+            </div>
+            <div className="content">
+                <h2 className="title">Intital Release 4/11</h2>
+                <p className="description">We have released four limited edition NFT's early which can be bid via  <a href="#">OpenSea.</a></p>
+                <p className="description">There will be the only four of NFT's we ever make, so be sure not to miss out!</p>
+                <p className="description">50% of proceeds go to charity.</p>
+                <a href="#" className="link">check them out <BsArrowRight /></a>
+            </div>
+        </div>
+    </div>
 }
--- src/components/SuperRare.jsx
+++ src/components/SuperRare.jsx
@@ -1,5 +1,64 @@
 import React from "react";
+import super1 from "../assets/super1.png";
+import super2 from "../assets/super2.png";
+import super3 from "../assets/super3.png";
+import super4 from "../assets/super4.png";
+import Card from "./Card";
 
 export default function SuperRare(){
-    return <div></div>;
+    const data = [
+        {
+            image: super1,
+            series: "Gloop series",
+            title: "Purple man",
+            price: 2.99,
+            tag: 12983,
+            time: 1,
+        },
+        {
+            image: super2,
+            series: "Gloop series",
+            title: "Purple man",
+            price: 2.99,
+            tag: 12983,
+            time: 1,
+        },
+        {
+            image: super3,
+            series: "Gloop series",
+            title: "Purple man",
+            price: 2.99,
+            tag: 12983,
+            time: 1,
+        },
+        {
+            image: super4,
+            series: "Gloop series",
+            title: "Purple man",
+            price: 2.99,
+            tag: 12983,
+            time: 1,
+        },
+    ];
+    return <div className="super-rare">
+        <div className="title-container">
+            <h2 className="title">
+                LE Super Rare Auction
+            </h2>
+            <p className="description">
+                We have released four limited edition NFT's early which can be bid via  <a href="#">OpenSea.</a>
+            </p>
+        </div>
+        <div className="cards">
+            {
+                data.map(({image, series, title, price, tag, time}, index)=>   <Card
+                image={image}
+                series={series}
+                title={title}
+                price={price} tag={tag} time={time}
+                key={index}
+                />)
+            }
+        </div>
+    </div>;
 }
